d2fa46cd3add1597cf8fcbc16d84fc9bb5b71ec4
RandyWiratamaa <randywiratama1@gmail.com>
2023-02-27 15:56:14 +0700

    Initialize

---
+++ database/migrations/2023_02_27_083856_create_detail_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('invoice_number');
+            $table->unsignedBigInteger('product_id');
+            $table->double('qty');
+            $table->double('subtotal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_orders');
+    }
+};
